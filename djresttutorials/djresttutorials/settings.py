04b6c8f1794f0e276a4db8cea948c3979f34e75e
Shishir Jha <shishirjhaccr@gmail.com>
2020-05-04 00:02:48 +0530

    done with the part 02 of the tutorial series

--- djresttutorials/djresttutorials/settings.py
+++ djresttutorials/djresttutorials/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'quickstart',
 ]
 
 MIDDLEWARE = [
@@ -113,6 +115,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Settings for rest_framework
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
